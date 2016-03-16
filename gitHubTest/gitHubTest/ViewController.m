7a087a6dee0393b2bb30d127802b3d2fc0dff373
linjiaqiao <649207624@qq.com>
2016-03-16 13:27:14 +0800

    Revert "Revert "Revert "first version"""
    This reverts commit 2a7c082dc20c5fc052460ba4220d695fb6b689d1.

--- gitHubTest/gitHubTest/ViewController.m
+++ gitHubTest/gitHubTest/ViewController.m
@@ -18,7 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"李四-first");
-    NSLog(@"林佳乔-firstVersion");
 }
 
 - (void)didReceiveMemoryWarning {
