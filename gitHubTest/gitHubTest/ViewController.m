187b78ff7ef7f37781bd5841123b4bb43fdd9739
xhw <835913166@qq.com>
2016-03-16 11:30:36 +0800

    大龙傻逼

--- gitHubTest/gitHubTest/ViewController.m
+++ gitHubTest/gitHubTest/ViewController.m
@@ -18,6 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"李四-first");
+    
+    
+    
+    NSLog(@"dalong傻逼");
 }
 
 - (void)didReceiveMemoryWarning {
